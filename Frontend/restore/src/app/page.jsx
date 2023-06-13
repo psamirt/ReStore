7e854e8d282ff0b5f9f893205c5d925ab447750f
Jere <jere_newells@hotmail.com>
2023-06-13 10:19:23 -0300

    feature: added components, routes and json example

---
+++ Frontend/restore/src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      landing soy landing page
+    </main>
+  )
+}
