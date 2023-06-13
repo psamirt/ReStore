0f2843ee1361cae61ddde7883071f80768c24b46
LucaFrisoni <frisoniluca1@gmail.com>
2023-06-13 16:29:25 -0300

    feat:Levantando Server

---
+++ Backend/src/Database/DB_connection.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { MONGO_URI } = process.env;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = connectDB;
