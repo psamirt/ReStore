8694ed6ff497a3607af61ff7cf9d020746105bff
LucaFrisoni <frisoniluca1@gmail.com>
2023-06-15 11:46:33 -0300

    feat:rutas put y get de disabled products

--- Backend/src/Routes/tecnologia.js
+++ Backend/src/Routes/tecnologia.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {postProduct,getAllCamarasyAccesorios,getAllCelulares,getAllComputacion,getAllConsolasyVideojuegos,getAllElectronicaAudioVideo,getAllTV} = require("../Controllers/controllersTecnologias")
+const {disabledProduct,getDisabledProducts} = require("../Controllers/disableProduct")
 
 
+//----------PostProducts---------------------------------------------------------------------------------------------------------------//
 router.post("/posteo",postProduct)
+
+//----------Disabled---------------------------------------------------------------------------------------------------------------//
+router.put("/disabled",disabledProduct)
+router.get("/disabled",getDisabledProducts)
+
+//----------getSubCategories---------------------------------------------------------------------------------------------------------------//
 router.get("/TV",getAllTV)
 router.get("/Computacion",getAllComputacion)
 router.get("/ElectronicaAudioVideo",getAllElectronicaAudioVideo)
