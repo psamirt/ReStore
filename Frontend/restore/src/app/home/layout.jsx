1e9b3a3381e1c287b12c2e6ff19f3fbc968aeb8d
Jere <jere_newells@hotmail.com>
2023-06-15 09:35:56 -0300

    navbar and home working

---
+++ Frontend/restore/src/app/home/layout.jsx
@@ -0,0 +1,11 @@
+import "./layout.css"
+
+export default function HomeLayout({ children }) {
+    return (
+      <html >
+        <body >
+          {children}
+          </body>
+      </html>
+    )
+  }
---
+++ Frontend/restore/src/app/user/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>soy el componente User</div>
+  )
+}
+
+export default page
