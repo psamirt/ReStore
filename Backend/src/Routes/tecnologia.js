1bb896e153c8d5d1bd17c16a0b4e8feac398ddc4
Jere <jere_newells@hotmail.com>
2023-06-15 22:38:43 -0300

    borrando las demas rutas subcategorias

--- Backend/src/Routes/tecnologia.js
+++ Backend/src/Routes/tecnologia.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {postProduct,getAllCamarasyAccesorios,getAllCelulares,getAllComputacion,getAllConsolasyVideojuegos,getAllElectronicaAudioVideo,getAllTV,getAllProducts,getAllProductsByCategory} = require("../Controllers/controllersTecnologias")
+const {postProduct,getAllProducts,getAllProductsByCategory} = require("../Controllers/controllersTecnologias")
 const {disabledProduct,getDisabledProducts} = require("../Controllers/disableProduct")
 const {ofertProduct,getOfertProducts} = require ("../Controllers/ofertas")
 const detailProduct = require("../Controllers/detail")
@@ -16,12 +16,8 @@ router.put("/disabled",disabledProduct)
 router.get("/disabled",getDisabledProducts)
 
 //----------getSubCategories---------------------------------------------------------------------------------------------------------------//
-router.get("/TV",getAllTV)
-router.get("/Computacion",getAllComputacion)
-router.get("/ElectronicaAudioVideo",getAllElectronicaAudioVideo)
-router.get("/ConsolasyVideojuegos",getAllConsolasyVideojuegos)
-router.get("/Celulares",getAllCelulares)
-router.get("/CamarasyAccesorios",getAllCamarasyAccesorios)
+router.get("/categoria/:category", getAllProductsByCategory)
+
 //----------Ofertas---------------------------------------------------------------------------------------------------------------//
 router.get("/Ofertas",getOfertProducts)
 router.put("/Ofertas",ofertProduct)
@@ -30,7 +26,6 @@ router.get("/Detail/:id",detailProduct)
 //-----------SearchByName-------------------------------------------------------------------------------------------------------------//
 router.get("/searchName",Search)
 
-router.get("/categoria/:category", getAllProductsByCategory)
 
 
 module.exports=router
