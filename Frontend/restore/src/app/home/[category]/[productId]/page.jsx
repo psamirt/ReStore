c83b245f4b574f14cccac08fc07636dd91f2829b
Jere <jere_newells@hotmail.com>
2023-06-19 18:35:43 -0300

    metadata, icons, slider

--- Frontend/restore/src/app/home/[category]/[productId]/page.jsx
+++ Frontend/restore/src/app/home/[category]/[productId]/page.jsx
@@ -1,16 +1,26 @@
 import { DetailId } from "./FetchDetail.jsx";
 import { Navbar } from "@/app/components/navbar/navbar.jsx";
+import { fetchDetail } from "../../fetch.js";
 
-async function Detail({params}) {
+export async function generateMetadata({ params, searchParams }, parent) {
+  const id = params.productId;
+
+  const product = await fetchDetail(id);
+
+  return {
+    title: product.result[0].name,
+  };
+}
+
+async function Detail({ params }) {
   return (
     <>
-    <Navbar/>
-<section>
-  <DetailId param={params.productId} />
-</section>
+      <Navbar />
+      <section>
+        <DetailId param={params.productId} />
+      </section>
     </>
   );
 }
 
 export default Detail;
-
--- Frontend/restore/src/app/layout.jsx
+++ Frontend/restore/src/app/layout.jsx
@@ -1,14 +1,24 @@
-import './globals.css';
-
+import "./globals.css";
 export const metadata = {
-  title: 'Store',
-  description: 'Encuentra los mejores productos en nuestro sitio!',
-  keywords: 'ecommerce, tienda en línea, productos, compras',
+  title: "ReStore",
+  description: "Encuentra los mejores productos en nuestro sitio!",
+  keywords: "ecommerce, tienda en línea, productos, compras",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple:[
+      "/apple-touch-icon.png?v=4"
+    ],
+    shortcut:[
+      "/apple-touch-icon.png"
+    ]
+  },
+  manifest: "/site.webmanifest"
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html className='bg-slate-100 text-slate-800 font-sans'>
+    <html className="bg-slate-100 text-slate-800 font-sans">
+      <head></head>
       <body>{children}</body>
     </html>
   );
