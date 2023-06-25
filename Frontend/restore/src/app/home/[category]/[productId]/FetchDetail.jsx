2ed5fc2030a952ff4176e34a8521f20c1409377c
psamirt <p_samir@hotmail.com>
2023-06-25 18:35:43 -0500

    Merge branch 'main' of https://github.com/psamirt/ReStore into deploy

--- Frontend/restore/src/app/home/[category]/[productId]/FetchDetail.jsx
+++ Frontend/restore/src/app/home/[category]/[productId]/FetchDetail.jsx
@@ -1,21 +1,22 @@
-'use client';
-import { fetchDetail } from '../../fetch';
-import Image from 'next/image';
-import Boton from '@/app/components/Button/Button';
-import BackButton from '@/app/components/backButton/BackButton';
-import NotFound from './notFound';
-import { addToCart } from '@/redux/actions';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import Loader from '@/app/components/loader/Loader';
-import { useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+"use client";
+import Image from "next/image";
+import Boton from "@/app/components/Button/Button";
+import BackButton from "@/app/components/backButton/BackButton";
+import NotFound from "./notFound";
+import { addToCart } from "@/redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import Loader from "@/app/components/loader/Loader";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import HomeContainer from "@/app/components/HomeContainer/HomeContainer";
 
 export function DetailId({ param }) {
   const dispatch = useDispatch();
   const { cart } = useSelector((store) => store);
   const { data: session, status } = useSession();
   const [post, setPost] = useState({ result: [] });
+  const [ofertas, setOfertas]= useState([])
   const [onCart, setOnCart] = useState(false);
 
   const [addedToCart, setAddedToCart] = useState(false);
@@ -31,6 +32,16 @@ export function DetailId({ param }) {
         `https://re-store.onrender.com/categories/technology/Detail/${param}`
       );
       setPost(response.data);
+    };
+    fetch();
+  }, []);
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(
+        `https://re-store.onrender.com/categories/technology/Ofertas`
+      );
+      setOfertas(response.data);
     };
     fetch();
   }, []);
@@ -65,74 +76,70 @@ export function DetailId({ param }) {
   const precioConDescuento = calculateDiscountedPrice();
 
   return (
-    <div className='container mx-auto px-4 my-8'>
+    <div className="container mx-auto px-4 my-8">
       {!post.result[0] ? (
         <Loader />
       ) : (
         <div>
           <BackButton />
-          <div className='grid md:grid-cols-2 gap-4 mb-12 mt-4'>
-            <div className='relative rounded-lg aspect-square shadow-md shadow-slate-200 md:justify-self-center '>
+          <div className="grid md:grid-cols-2 gap-4 mb-12 mt-4">
+            <div className="relative rounded-lg aspect-square shadow-md shadow-slate-200 md:justify-self-center ">
               <Image
-                className='aspect-square rounded-lg object-contain'
+                className="aspect-square rounded-lg object-contain"
                 src={post.result[0].background_image}
                 alt={post.result[0].name}
                 fill
               />
             </div>
-            <div className='grid gap-4 text-gray-600'>
-              <h2 className='text-xl font-semibold text-blue-900'>
+            <div className="grid gap-4 text-gray-600">
+              <h2 className="text-xl font-semibold text-blue-900">
                 {post.result[0].name} {post.result[0].Marca}
               </h2>
-              <p className=''>Ubicacion : {post.result[0].Ubicacion}</p>
-              <p className=''>Estado: {post.result[0].state}</p>
-
+              <p className="">Ubicacion : {post.result[0].Ubicacion}</p>
+              <p className="">Estado: {post.result[0].state}</p>
+              <h3 className="">
+                Calificación del vendedor : <img src="" alt="" />5
+              </h3>
               {precioConDescuento ? (
-                <p className='font-medium text-base text-slate-500'>
-                  Precio:{' '}
-                  <span className='text-red-400 line-through'>
+                <p className="font-medium text-base text-slate-500">
+                  Precio:{" "}
+                  <span className="text-red-400 line-through">
                     ${post.result[0].precio}
-                  </span>{' '}
-                  <span className='text-slate-800'>${precioConDescuento}</span>
+                  </span>{" "}
+                  <span className="text-slate-800">${precioConDescuento}</span>
                 </p>
               ) : (
-                <p className='font-medium text-base text-slate-800'>
+                <p className="font-medium text-base text-slate-800">
                   Precio: ${post.result[0].precio}
                 </p>
               )}
-              <h3 className=''>
-                Calificación del vendedor : <img src='' alt='' />5
-              </h3>
-              <p className='text-blue-900 text-xl font-semibold'>
-                Precio: ${post.result[0].precio}
-              </p>
               <div>
-                <label className='block mb-2'>Método de envio :</label>
+                <label className="block mb-2">Método de envio :</label>
                 <select
-                  className='cursor-pointer py-1 px-2 rounded-lg bg-slate-300    text-slate-800 font-medium'
-                  name='metodo'
-                  id='envio'
+                  className="cursor-pointer py-1 px-2 rounded-lg bg-slate-300    text-slate-800 font-medium"
+                  name="metodo"
+                  id="envio"
                 >
-                  <option value='1'>Opción 1</option>
-                  <option value='2'>Opción 2</option>
+                  <option value="1">Opción 1</option>
+                  <option value="2">Opción 2</option>
                 </select>
               </div>
-              <div className='flex gap-4 align-middle'>
-                <span className='justify-self-start'>
+              <div className="flex gap-4 align-middle">
+                <span className="justify-self-start">
                   <Boton
                     onClick={() => handleAddToCart()}
-                    text={'Añadir al carrito'}
+                    text={"Añadir al carrito"}
                   >
                     Añadir al carrito
                   </Boton>
                 </span>
                 {addedToCart ? (
                   !onCart ? (
-                    <p className='self-center font-medium text-green-500'>
+                    <p className="self-center font-medium text-green-500">
                       Agregado exitósamente
                     </p>
                   ) : (
-                    <p className='self-center font-medium text-red-500'>
+                    <p className="self-center font-medium text-red-500">
                       Ya agregaste este producto al carrito
                     </p>
                   )
@@ -140,24 +147,20 @@ export function DetailId({ param }) {
               </div>
             </div>
           </div>
-          <div className='grid  gap-4 mb-8'>
-            <div className='grid gap-2'>
-              <h3 className='text-blue-900 text-xl font-medium'>
-                Descripción:{' '}
+          <div className="grid  gap-4 mb-8">
+            <div className="grid gap-2">
+              <h3 className="text-blue-900 text-xl font-medium">
+                Descripción:{" "}
               </h3>
-              <p className='text-gray-600 leading-normal text-sm'>
+              <p className="text-gray-600 leading-normal text-sm">
                 {post.result[0].Description}
               </p>
             </div>
-            <div className='grid gap-2'>
-              <h3 className='text-xl font-semibold text-blue-900'>
+            <div className="grid gap-2">
+              <h3 className="text-xl font-semibold text-blue-900">
                 También te puede interesar:
               </h3>
-              <img
-                className=''
-                src='https://i.stack.imgur.com/t0k67.png'
-                alt='imagen referencial'
-              />
+              <HomeContainer data={ofertas}></HomeContainer>
             </div>
           </div>
         </div>
