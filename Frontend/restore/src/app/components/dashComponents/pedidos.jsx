00af49b3d17b973301026234d9ea91d2f67238d5
Jere <jere_newells@hotmail.com>
2023-06-27 20:37:13 -0300

    dashboard

--- Frontend/restore/src/app/components/dashComponents/pedidos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Pedidoss() {
-    return (
-      <div>Pedidoss</div>
-    )
-  }
-  
-  export default Pedidoss
--- Frontend/restore/src/app/dashboardadmin/clientes/page.jsx
+++ Frontend/restore/src/app/dashboardadmin/clientes/page.jsx
@@ -1,21 +1,12 @@
 "use client"
 import React from 'react'
 import '../dash.css'
-import DashHeader from "../../components/dashHeader/b"
-import DashSideMenu from "../../components/dashSideMenu/d";
-import DashPage from "../../components/dashPageContent/c";
-import DashFooter from "../../components/dashFooter/a";
-import { Space } from "antd";
+
 
 function Clientes() {
   return (
     <div className="app">
-    <DashHeader />
-    <Space className="SideMenuAndPageContent">
-      <DashSideMenu />
-      <DashPage render={"clientes"}/>
-    </Space>
-    <DashFooter />
+      clientes
   </div>
   )
 }
