08776ab083c4ecbcfcc33f910bbd9ba5f4ecf3fe
LucaVera13 <VeraLuuca@gmail.com>
2023-06-27 08:38:22 -0300

    pagos no terminados pero para implementar boton en el carrito
    pagos no terminados pero para implementar boton en el carrito

--- Backend/src/Database/models/userModel.js
+++ Backend/src/Database/models/userModel.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
   nombre: {
@@ -21,7 +21,7 @@ const userSchema = new Schema({
   },
   genero: {
     type: String,
-    enum: ['Masculino', 'Femenino', 'Otro'],
+    enum: ["Masculino", "Femenino", "Otro"],
   },
   fechaNacimiento: {
     type: Date,
@@ -42,8 +42,7 @@ const userSchema = new Schema({
   carrito: [
     {
       productId: {
-        type: String, // Suponiendo que los productos tienen un ObjectId único
-        // Nombre del modelo de los productos
+        type: String,
       },
       cantidad: {
         type: Number,
@@ -69,4 +68,4 @@ const userSchema = new Schema({
   ],
 });
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
--- Backend/src/Routes/index.js
+++ Backend/src/Routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const categoryTechnology = require("./tecnologia");
 const userRouter = require("./userRouter");
 const carritoRouter = require("./carrito");
+const rutaPagos = require("./rutaPagos");
 
 router.use("/categories/technology", categoryTechnology);
 router.use("/users", userRouter);
 router.use("/carrito", carritoRouter);
+router.use("/create-checkout-session", rutaPagos);
 
 module.exports = router;
