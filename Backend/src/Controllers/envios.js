51e0922009cf000333a1259a337222111b8cb62b
LucaVera13 <VeraLuuca@gmail.com>
2023-07-04 16:54:17 -0300

    Arreglo ordenes recientes
    Ya funciona ordenes recientes

--- Backend/src/Controllers/envios.js
+++ Backend/src/Controllers/envios.js
@@ -2,10 +2,14 @@ const User = require("../Database/models/userModel");
 
 const getUsersOrders = async (req, res) => {
   try {
-    const users = await User.find({ "orders": { $exists: true, $ne: [] } });
+    const users = await User.find({ orders: { $exists: true, $ne: [] } })
+      .sort({ "orders.createdAt": -1 })
+      .populate("orders.customerId"); // Agregar populate para obtener los detalles del cliente en cada orden
 
     if (!users || users.length === 0) {
-      return res.status(404).json({ error: "No se encontraron usuarios con órdenes" });
+      return res
+        .status(404)
+        .json({ error: "No se encontraron usuarios con órdenes" });
     }
 
     const usersData = users.map((user) => ({
@@ -18,7 +22,9 @@ const getUsersOrders = async (req, res) => {
 
     res.status(200).json({ users: usersData });
   } catch (error) {
-    res.status(500).json({ error: "Error al obtener los usuarios y sus órdenes" });
+    res
+      .status(500)
+      .json({ error: "Error al obtener los usuarios y sus órdenes" });
   }
 };
 
